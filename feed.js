599778f7e5d9bcefc11fa833a8d787b9392762d1
iarigby <marvinzem@gmail.com>
2019-09-01 22:38:48 +0400

    pre refactor

--- feed.js
+++ feed.js
@@ -1,4 +1,57 @@
-import { main } from './'
+function main(response) {
+    console.log(`received response of ${response.data.length}`) 
+    
+    let i = 0
+    for (post of response.data) {
+        i++
+        // post.message and post.
+        console.log(post.from)
+        setTimeout(function() {
+            FB.api(
+            `/${post.id}/comments`,
+            function (response) {
+                if (response && !response.error) {
+                    get_comments(response.data)
+                    /* handle the result */
+                } else {
+                    console.log(response.error)
+                }
+            }
+            )}, i*100)
+    }
+}
+
+function get_comments(comments) {
+    for (comment of comments) {
+        FB.api(
+            `/${comment.id}`,
+            function (response) {
+                if (response && !response.error) {
+                    console.log(response)
+                }
+            }
+        )
+    }
+}
+
+function send_delayed_request(arr, endpoint_function) {
+    let i = 0
+    for (el of arr) {
+        i++
+        setTimeout(function(el) {
+            FB.api(
+                endpoint_function(el),
+                function(response) {
+                    if (response && !response.error) {
+                        callback(response)
+                    } else {
+                        console.log(response.error)
+                    }
+                })
+        }(el), i*500)
+    }
+}
+// fb crap
 
 window.fbAsyncInit = function() {
     FB.init({
@@ -14,16 +67,19 @@ window.fbAsyncInit = function() {
             /**
                Also keep in mind that the access tokens that are generated in browsers generally have a lifetime of only a couple of hours and are automatically refreshed by the JavaScript SDK. If you are making calls from a server, you will need to generate a long lived token, which is covered at length in our access token documentation.
             **/
-            console.log(response.authResponse.accessToken);
-            FB.api('/me', function(response) {
-                console.log(JSON.stringify(response));
-            }); 
+            /*
+              console.log(response.authResponse.accessToken);
+              FB.api('/me', function(response) {
+              console.log(JSON.stringify(response));
+              });
+            */
             FB.api(
                 "/1473567402914154/feed",
                 function (response) {
                     if (response && !response.error) {
-                        /* handle the result */
-                        console.log(response)
+                        main(response)
+                    } else {
+                        console.log(response.error)
                     }
                 }
             );
